218753524230d93651e7aebe0c7677561cb2d4e8
hzq <huang_zuqing@itrus.com.cn>
2020-03-31 10:25:21 +0800

    excel导入公共接口

---
+++ src/main/java/com/itrus/common/utils/excel/FieldForSortting.java
@@ -0,0 +1,64 @@
+package com.itrus.common.utils.excel;
+
+import java.lang.reflect.Field;
+
+
+/**
+ * <p>
+ * excel 字段
+ * </p>
+ *
+ * @author hzq
+ * @since 2020/3/30
+ */
+public class FieldForSortting {
+    private Field field;
+    private int index;
+
+    /**
+     * @param field
+     */
+    public FieldForSortting(Field field) {
+        super();
+        this.field = field;
+    }
+
+    /**
+     * @param field
+     * @param index
+     */
+    public FieldForSortting(Field field, int index) {
+        super();
+        this.field = field;
+        this.index = index;
+    }
+
+    /**
+     * @return the field
+     */
+    public Field getField() {
+        return field;
+    }
+
+    /**
+     * @param field the field to set
+     */
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    /**
+     * @return the index
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    /**
+     * @param index the index to set
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+}
