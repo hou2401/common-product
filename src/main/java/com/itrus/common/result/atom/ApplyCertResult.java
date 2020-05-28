0e05a6f5970239fa2d50ea0e674e8675344c0355
wgx521 <wgx521@139.com>
2020-05-28 12:55:56 +0800

    证书返回实体，添加返回参数注释

--- src/main/java/com/itrus/common/result/atom/ApplyCertResult.java
+++ src/main/java/com/itrus/common/result/atom/ApplyCertResult.java
@@ -1,7 +1,5 @@
 package com.itrus.common.result.atom;
 
-import java.util.Date;
-
 import lombok.Data;
 import lombok.ToString;
 
@@ -9,23 +7,57 @@ import lombok.ToString;
 @ToString
 public class ApplyCertResult{
 	
+	/**
+	 * 证书ID
+	 */
 	private Long id;
 	
+	/**
+	 * 证书序列号
+	 */
 	private String certSn;
-	
+	/**
+	 * 证书Base64编码
+	 */
 	private String certBuf;
 	
+	/**
+	 * 证书链
+	 */
 	private String certChain;
 	
+	/**
+	 * 证书签发者
+	 */
 	private String  issuer;
 	
+	/**
+	 * 证书主题
+	 */
 	private String subject;
 	
-	private Date notAfter;
+	/**
+	 * 证书生效时间，日期格式：yyyy-MM-dd HH:mm:ss
+	 */
+	private String notBefore;
+
+	/**
+	 * 证书过期时间，日期格式：yyyy-MM-dd HH:mm:ss
+	 */
+	private String notAfter;
 	
-	private Date notBefore;
-	
+	/**
+	 * 密钥算法
+	 */
 	private String privateKeyAlg;
 	
+	/**
+	 * 证书状态 1:有效，2:吊销，3：已过期
+	 */
 	private Integer status ;
+	
+	/**
+	 * SM3哈希算法参数Z值，SM2证书时返回
+	 */
+	private String sm3zValue;
 }
