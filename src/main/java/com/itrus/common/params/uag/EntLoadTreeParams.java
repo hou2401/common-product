a8568b74cd3de535404cd8b3fcb73d0548ac9978
li_jiasen <li_jiasen@itrus.com.cn>
2020-06-03 16:47:14 +0800

    修改初始化 组织树调用接口

--- src/main/java/com/itrus/common/params/uag/EntLoadTreeParams.java
+++ src/main/java/com/itrus/common/params/uag/EntLoadTreeParams.java
@@ -4,8 +4,6 @@ import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.ToString;
 
-import java.util.Objects;
-
 @ApiModel(description = "请求UAG查询加载企业树实体")
 @ToString
 @Data
@@ -21,59 +19,15 @@ public class EntLoadTreeParams {
     private String manager;
 
     /**
-     * 用户姓名
+     * 组合查询条件
      */
-    private String displayName;
-
-    /**
-     * 默认需要显示超级管理员
-     * true显示
-     */
-    private Boolean ifRoot;
-
-    /**
-     * 加入状态
-     */
-    private String[] joinState;
-
-    /**
-     * 查询节点类型  用户/部门/公司
-     */
-    private String[] nodeType;  // 查询节点类型   1：用户  2：部门 3：公司
-
-    /**
-     * 审核条件   待审核/审核通过/审核拒绝 --check_status
-     */
-    private String[] ifAudited;   // 审核状态 1：审核通过  3：待审核  4：审核拒绝  不填默认查询全部
-
-    /**
-     * 用户认证状态
-     */
-    private String realName;  // 可选 0未实名 1已实名
-
+   private SearchConditionloadTreeParams searchCondition;
     public EntLoadTreeParams() {
     }
 
-    public EntLoadTreeParams(Long uuid, String manager, String[] nodeType) {
+    public EntLoadTreeParams(Long uuid, String manager, SearchConditionloadTreeParams searchCondition) {
         this.uuid = uuid;
         this.manager = manager;
-        this.nodeType = nodeType;
-    }
-
-    public EntLoadTreeParams(Long uuid, String manager, Boolean ifRoot, String[] joinState, String[] ifAudited, String realName) {
-        this.uuid = uuid;
-        this.manager = manager;
-        this.ifRoot = ifRoot;
-        this.joinState = joinState;
-        this.ifAudited = ifAudited;
-        this.realName = realName;
-    }
-
-    public EntLoadTreeParams(Long uuid, String manager, String[] joinState, String[] ifAudited, String realName) {
-        this.uuid = uuid;
-        this.manager = manager;
-        this.joinState = joinState;
-        this.ifAudited = ifAudited;
-        this.realName = realName;
+        this.searchCondition = searchCondition;
     }
 }
