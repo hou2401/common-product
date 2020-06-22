bea7635653268c1fd02937a51c122aeca80b45af
韩延辉 <han_yanhui@itrus.com.cn>
2020-06-22 18:09:59 +0800

    优化代码

---
+++ src/main/java/com/itrus/common/result/uag/dto/enterprise/EnterpriseDetail.java
@@ -0,0 +1,52 @@
+package com.itrus.common.result.uag.dto.enterprise;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class EnterpriseDetail implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 企业uuid
+	 */
+	private Long uuid;
+	
+	/**
+	 * 企业全称
+	 */
+	private String orgName;
+	
+	/**
+	 * 企业信用代码
+	 */
+	private String orgCode;
+	
+	/**
+	 * 简称集合实体
+	 */
+	private String orgAlias;
+	
+	/**
+	 * 简称集合实体
+	 */
+	private String ifAudited;
+	
+	/**
+	 * 简称集合实体
+	 */
+	private String realName;
+	
+	/**
+	 * 简称集合实体
+	 */
+	private Long parentUuid;
+	
+
+}
