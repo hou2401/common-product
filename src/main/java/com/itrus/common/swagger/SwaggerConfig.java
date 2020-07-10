c0d3741173d759237e305978626ee901b4e6ccb1
wgx521 <wgx521@139.com>
2020-07-10 13:53:50 +0800

    增加 swagger 样例

--- src/main/java/com/itrus/common/swagger/SwaggerConfig.java
+++ src/main/java/com/itrus/common/swagger/SwaggerConfig.java
@@ -2,45 +2,55 @@ package com.itrus.common.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
-* <p>Title: SwaggerConfig.java</p>
-* <p>Description: swagger2 配置类</p>
-* <p>Copyright: Copyright (c) 2007</p>
-* <p>Company: com.itrus</p>
-* @author wen_guoxing
-* @date 2019年8月9日
-* @version 1.0
+ * <p>Title: SwaggerConfig.java</p>
+ * <p>Description: swagger2 配置类</p>
+ * <p>Copyright: Copyright (c) 2007</p>
+ * <p>Company: com.itrus</p>
+ * @author wen_guoxing
+ * @date 2019年8月9日
+ * @version 1.0
  */
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                // 自行修改为自己的包路径
-                .apis(RequestHandlerSelectors.basePackage("com.itrus.demo.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("api文档")
-                .description("restful 风格接口")
-                //服务条款网址
-                //.termsOfServiceUrl("")
-                .version("1.0")
-                //.contact(new Contact("hello", "url", "email"))
-                .build();
-    }
+@Configuration
+
+@EnableSwagger2 
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	@Bean 
+	public Docket api() { return new Docket(DocumentationType.SWAGGER_2)
+			.apiInfo(this.apiInfo()) .select() // 自行修改为自己的包路径
+			.apis(RequestHandlerSelectors.basePackage("com.itrus.common.http"))
+			.paths(PathSelectors.any()) .build(); 
+	}
+	private ApiInfo apiInfo() { 
+		return	new ApiInfoBuilder().title("api文档") 
+				.description("restful 风格接口") //服务条款网址
+				.termsOfServiceUrl("") 
+				.version("2.0") 
+				.contact(new Contact("hello","url", "email")) 
+				.build();
+	}
+	
+	@Override 
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+		.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+	}
 }
+
