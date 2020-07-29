8bb6987c2252e42cafb20673d10e7ccef9d0c038
wgx521 <wgx521@139.com>
2020-07-29 14:42:05 +0800

    log日志修改，swagger文档修改

--- src/main/java/com/itrus/common/swagger/SwaggerConfig.java
+++ src/main/java/com/itrus/common/swagger/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.itrus.common.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -24,23 +24,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @version 1.0
  */
 
+@SuppressWarnings("deprecation")
 @Configuration
 
 @EnableSwagger2 
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
 	@Bean 
 	public Docket api() { return new Docket(DocumentationType.SWAGGER_2)
 			.apiInfo(this.apiInfo()) .select() // 自行修改为自己的包路径
-			.apis(RequestHandlerSelectors.basePackage("com.itrus.common.http"))
+			.apis(RequestHandlerSelectors.basePackage("com.itrus.common.controller"))
 			.paths(PathSelectors.any()) .build(); 
 	}
 	private ApiInfo apiInfo() { 
 		return	new ApiInfoBuilder().title("api文档") 
 				.description("restful 风格接口") //服务条款网址
 				.termsOfServiceUrl("") 
-				.version("2.0") 
+				.version("2.2.0") 
 				.contact(new Contact("hello","url", "email")) 
+				.license("The Apache License")
+                .licenseUrl("http://www.baidu.com")
 				.build();
 	}
 	
