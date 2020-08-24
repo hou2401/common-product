81737ffbadf54a63b7dbeb313ec3a75746c2a91b
yu_ying <yu_ying@itrus.com.cn>
2020-08-24 10:31:17 +0800

    提交企业公共类

--- src/main/java/com/itrus/common/uag/request/base/EntBase.java
+++ src/main/java/com/itrus/common/uag/request/base/EntBase.java
@@ -1,9 +1,39 @@
 package com.itrus.common.uag.request.base;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EntBase {
+    /**
+     * 企业唯一标识
+     */
+    private Long companyUuid;
+    /**
+     * 企业全称
+     */
+    private String orgName;
+    /**
+     * 是否三证合一
+     */
+    private String inOneType;
+    /**
+     * 统一社会信用代码（三证合一必选）
+     */
+    private String socialCode;
+    /**
+     * 组织结构代码（三证未合一必选）
+     */
+    private String unitCode;
+    /**
+     * 工商注册号（三证未合一必选）
+     */
+    private String registerNumber;
+
+
 }
