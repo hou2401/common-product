29e1bf2912465c0557d9a4f9b80f793b0ef8b555
yu_ying <yu_ying@itrus.com.cn>
2020-08-24 11:21:37 +0800

    提交应用base类

---
+++ src/main/java/com/itrus/common/uag/request/base/AppBase.java
@@ -0,0 +1,25 @@
+package com.itrus.common.uag.request.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppBase {
+    /**
+     * 应用id
+     */
+    private String appId;
+    /**
+     * 应用名称
+     */
+    private String appName;
+    /**
+     * 应用key
+     */
+    private String appKey;
+}
