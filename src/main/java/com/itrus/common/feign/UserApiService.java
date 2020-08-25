14d0f70c8d427b1af3ad5a4f6eb2cd6bd1e04360
yu_ying <yu_ying@itrus.com.cn>
2020-08-25 21:16:41 +0800

    nacos注册中心成功版

--- src/main/java/com/itrus/common/feign/UserApiService.java
+++ src/main/java/com/itrus/common/feign/UserApiService.java
@@ -1,28 +1,28 @@
-package com.itrus.common.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.itrus.common.params.uag.user.RegisterParams;
-import com.itrus.common.result.uag.UagResult;
-import com.itrus.common.result.uag.user.RegisterResult;
-
-@FeignClient("userapi")
-public interface UserApiService {
-
-	/**
-	 * 	用户注册
-	    * @api {POST} /user/register [用户注册]
-	    * @apiVersion version
-	    * @apiUse RegisterParams
-	    * @apiName  register
-	    * @apiGroup userapi
-	    * @apiSuccess [(group)] [{type}] field [description]
-	 */
-	@PostMapping("/user/register")
-	UagResult<RegisterResult> register(RegisterParams params);
-
-	
-	
-}
+//package com.itrus.common.feign;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import com.itrus.common.params.uag.user.RegisterParams;
+//import com.itrus.common.result.uag.UagResult;
+//import com.itrus.common.result.uag.user.RegisterResult;
+//
+////@FeignClient("userapi")
+//public interface UserApiService {
+//
+//	/**
+//	 * 	用户注册
+//	    * @api {POST} /user/register [用户注册]
+//	    * @apiVersion version
+//	    * @apiUse RegisterParams
+//	    * @apiName  register
+//	    * @apiGroup userapi
+//	    * @apiSuccess [(group)] [{type}] field [description]
+//	 */
+//	@PostMapping("/user/register")
+//	UagResult<RegisterResult> register(RegisterParams params);
+//
+//
+//
+//}
