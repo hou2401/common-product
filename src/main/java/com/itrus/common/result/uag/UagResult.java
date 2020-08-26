d1f250130587bf22212cb5628fa2c1027d21278e
lijiasen <1225801556@qq.com>
2020-08-26 13:59:42 +0800

    添加公共对uag返回参数处理

--- src/main/java/com/itrus/common/result/uag/UagResult.java
+++ src/main/java/com/itrus/common/result/uag/UagResult.java
@@ -2,10 +2,12 @@ package com.itrus.common.result.uag;
 
 import java.io.Serializable;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
 import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 @ToString
@@ -15,6 +17,10 @@ public class UagResult<T> implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	private static final String CODE_KEY = "code";
+	private static final Integer GET_CODE = 0;
+	public static final String defaultMessage = "调用UAG失败";
 
 	/**
 	 * 返回状态
@@ -28,5 +34,42 @@ public class UagResult<T> implements Serializable{
 	
 	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private T data;
-	
+
+	/**
+	 *     请求OK
+	 *
+	 * @param result json对象
+	 */
+	public static boolean isOk(JSONObject result) {
+		if(result == null ){
+			return false;
+		}
+		return  GET_CODE.equals(result.get(CODE_KEY)) ? true : false;
+	}
+
+	/**
+	 *     请求不OK
+	 *
+	 * @param result json对象
+	 */
+	public static boolean isNotOk(JSONObject result) {
+		return !isOk(result);
+	}
+
+
+	/**
+	 * 获取响应失败提示语
+	 */
+	public static String getMessage(JSONObject jsonObject) {
+		if(jsonObject == null) {
+			return defaultMessage;
+		}
+		String string = jsonObject.getString("msg");
+		if(StringUtils.isNotBlank(string)) {
+			return string;
+		}
+		return defaultMessage;
+	}
+
+
 }
