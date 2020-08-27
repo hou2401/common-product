b23fbef0356df367ee776c8b646c3e2cdba6bd0c
韩延辉 <han_yanhui@itrus.com.cn>
2020-08-27 16:31:21 +0800

    优化代码

--- src/main/java/com/itrus/common/http/CertApiRequest.java
+++ src/main/java/com/itrus/common/http/CertApiRequest.java
@@ -1,14 +1,13 @@
 package com.itrus.common.http;
 
 import com.itrus.common.request.uag.cert.CertUpdateRequest;
-import com.itrus.common.response.cert.CertApplyResult;
+import com.itrus.common.response.cert.ApplyCertResult;
 import com.itrus.common.response.cert.CertUpdateResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.alibaba.fastjson.JSONObject;
 import cn.com.itrus.atom.sign.common.bean.Result;
 /**
  * 证书原子服务调用方法
@@ -28,7 +27,7 @@ public interface CertApiRequest {
 	 * @throws Exception 抛出异常
 	 */
 	@RequestMapping(value = "/cert/apply", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Result<CertApplyResult> applyCert(Object obj);
+	public Result<ApplyCertResult> applyCert(Object obj);
 
 	/**
 	 * 更新证书
