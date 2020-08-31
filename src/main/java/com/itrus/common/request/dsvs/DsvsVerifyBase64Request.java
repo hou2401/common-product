23eb27a8e06fd17a53da186a4f9b3c95a016ed78
menglingle <meng_lingle@itrus.com.cn>
2020-08-31 11:15:18 +0800

    删除冗余代码

--- src/main/java/com/itrus/common/request/dsvs/DsvsVerifyBase64Request.java
+++ src/main/java/com/itrus/common/request/dsvs/DsvsVerifyBase64Request.java
@@ -1,6 +1,5 @@
 package com.itrus.common.request.dsvs;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
