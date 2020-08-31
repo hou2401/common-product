2835dd50181b69f4747c8e595e51c5dddae25810
yu_ying <yu_ying@itrus.com.cn>
2020-08-31 12:45:55 +0800

    添加系统配置Feign接口

---
+++ src/main/java/com/itrus/common/http/RaApiRequest.java
@@ -0,0 +1,123 @@
+package com.itrus.common.http;
+
+import cn.com.itrus.atom.sign.common.bean.Result;
+import com.itrus.common.request.ra.CertConfigRequest;
+import com.itrus.common.request.ra.TimeStampRequest;
+import com.itrus.common.response.ra.CertConfigResponse;
+import com.itrus.common.response.ra.RaResult;
+import com.itrus.common.response.ra.TimeStampResponse;
+import org.bouncycastle.asn1.tsp.TimeStampReq;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.redis.core.script.ReactiveScriptExecutor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * @author yuying
+ */
+@Component
+@FeignClient(value = "${http.ra}") //value:被调用的服务在注册中心的名称
+public interface RaApiRequest {
+    /**
+     * 新建RA证书配置
+     *
+     * @param obj
+     * @return
+     */
+    @RequestMapping(value = "/web/ra/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> RaAdd(CertConfigRequest obj);
+
+    /**
+     * 查询RA证书配置
+     *
+     * @param raCode
+     * @return
+     */
+    @RequestMapping(value = "/web/ra/getByRaCode", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> GetByRaCode(@RequestPart("raCode") String raCode);
+
+
+    /**
+     * 更新RA证书配置
+     *
+     * @param obj
+     * @return
+     */
+    @RequestMapping(value = "/web/ra/update", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> RaUpdate(CertConfigRequest obj);
+
+
+    /**
+     * 根据唯一标识删除区块链服务配置
+     *
+     * @param raCode
+     * @return
+     */
+    @RequestMapping(value = "/web/ra/delete", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> RaDelete(@RequestPart("raCode") String raCode);
+
+    /**
+     * 查询区块链服务配置列表
+     *
+     * @param enterpriseId 必选 企业唯一标识
+     * @param productId    可选 产品唯一标识
+     * @return
+     */
+    @RequestMapping(value = "/web/ra/getList", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<List<CertConfigResponse>> RaGetList(@RequestPart("enterpriseId") String enterpriseId, @RequestPart("productId") String productId);
+
+
+    /**
+     * 新增时间戳配置
+     *
+     * @param obj
+     * @return
+     */
+    @RequestMapping(value = "/web/tsa/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> TsaAdd(TimeStampRequest obj);
+
+    /**
+     * 根据企业唯一标识查询时间戳配置详情，只返回最后一次添加的一条
+     *
+     * @param enterpriseId 必填 企业唯一标识
+     * @param productId    可选 产品唯一标识
+     * @return
+     */
+    @RequestMapping(value = "/web/tsa/getTsaConfig", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<TimeStampResponse> TsaAdd(@RequestPart("enterpriseId") String enterpriseId, @RequestPart("productId") String productId);
+
+    /**
+     * 根据tsaCode查询时间戳配置是否已存在
+     *
+     * @param tsaCode 必选 时间戳代码
+     * @return
+     */
+
+    @RequestMapping(value = "/web/tsa/getByTsaCode", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<TimeStampResponse> TsaGetByTsaCode(@RequestPart("tsaCode") String tsaCode);
+
+    /**
+     * 根据tsaCode更新时间戳配置
+     *
+     * @param obj
+     * @return
+     */
+    @RequestMapping(value = "/web/tsa/update", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> TsaUpdate(TimeStampRequest obj);
+
+    /**
+     * 根据tsaCode删除时间戳服务配置
+     *
+     * @param tsaCode 必填 时间戳代码
+     * @return
+     */
+    @RequestMapping(value = "/web/tsa/delete", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    RaResult<String> TsaDelete(@RequestPart("tsaCode") String tsaCode);
+
+}
---
+++ src/main/java/com/itrus/common/response/ra/CertConfigResponse.java
@@ -0,0 +1,133 @@
+package com.itrus.common.response.ra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author yuying
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertConfigResponse implements Serializable {
+    private static final long serialVersionUID = 482421873297316726L;
+    /**
+     * raType  ra类型
+     */
+    @NotNull(message = "RA类型不允许为空")
+    @Min(value = 1, message = "ra类型只能是1或者2")
+    @Max(value = 2, message = "ra类型只能是1或者2")
+    private int raType;
+
+    /**
+     * 证书名称
+     */
+    @NotBlank(message = "证书名称不允许为空")
+    @Range(min = 1, max = 100, message = "证书名称在1-100之间")
+    private String raName;
+
+    /**
+     * raCode
+     */
+    @NotBlank(message = "RaCode不允许为空")
+    @Range(min = 1, max = 100, message = "RaCode在1-100之间")
+    private String raCode;
+
+    /**
+     * 证书类型
+     */
+    @NotNull(message = "证书类型不允许为空")
+    @Min(value = 1, message = "证书类型是1或者2")
+    @Max(value = 2, message = "证书类型是1或者2")
+    private int certType;
+
+    /**
+     * 证书子类
+     */
+    @NotNull(message = "证书子类型不允许为空")
+    @Min(value = 1, message = "证书子类型是1或者2")
+    @Max(value = 2, message = "证书子类型是1或者2")
+    private int certSubType;
+
+    /**
+     * 证书算法
+     */
+    /**
+     * raCode
+     */
+    @NotBlank(message = "证书算法不允许为空")
+    @Range(min = 1, max = 20, message = "证书算法在1-20之间")
+    private String keyAlgorithm;
+
+    /**
+     * ra地址
+     */
+//	@NotBlank(message = "ra地址不能为空")
+    @Range(min = 1, max = 255, message = "ra地址在1-255之间")
+    private String serviceUrl;
+
+    /**
+     * ra账号
+     */
+//	@NotBlank(message = "ra账号hash不能为空")
+    @Range(min = 1, max = 255, message = "ra账号hash在1-255之间")
+    private String raAccountHash;
+    /**
+     * ra密码
+     */
+//	@NotBlank(message = "RA密码不能为空")
+    @Range(min = 1, max = 255, message = "ra密码在1-255之间")
+    private String aaPassword;
+
+    /**
+     * 应用标识
+     */
+//	@NotBlank(message = "appId不能为空")
+    @Range(min = 1, max = 50, message = "appId在1-50之间")
+    private String appId;
+
+    /**
+     * secretkey
+     */
+//	@NotBlank(message = "secretkey不能为空")
+    @Range(min = 1, max = 50, message = "ra密码在1-50之间")
+    private String appSecret;
+
+    /**
+     * serviceCode
+     */
+//	@NotBlank(message = "serviceCode不能为空")
+    @Range(min = 1, max = 50, message = "serviceCode  1-50之间")
+    private String serviceCode;
+    /**
+     * 有效期
+     */
+    private String maxValidDays;
+
+    /**
+     * 产品标识
+     */
+    private String productId;
+
+    /**
+     * 企业标识
+     */
+    private String enterpriseId;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+}
