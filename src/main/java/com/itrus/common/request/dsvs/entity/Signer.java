2c9f59b182790d5b691157ce2d0812c201952c9e
hzq <huang_zuqing@itrus.com.cn>
2020-09-03 10:27:52 +0800

    签署接口PdfSign请求对象新增tsaCode请求参数

--- src/main/java/com/itrus/common/request/dsvs/entity/Signer.java
+++ src/main/java/com/itrus/common/request/dsvs/entity/Signer.java
@@ -38,6 +38,11 @@ public class Signer implements Serializable {
      * 是否加盖时间戳，默认false不加盖
      */
     private Boolean timestamp;
+
+    /**
+     * 时间戳代码，用于选择使用哪个时间戳服务<br/>不填时使用默认的时间戳服务
+     */
+    private String tsaCode;
     /**
      * 签名策略： <br/>1：只有第一个印章图片添加数字签名，<br />      其它印章添加水印（首次签章默认）<br/>2：所有印章图片都添加签名（非首次签章默认） <br/>3：所有印章图片都不添加数字签名，无
      */
