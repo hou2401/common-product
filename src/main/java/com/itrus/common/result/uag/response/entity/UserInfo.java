f7c70117d537c03ca9a18051523d9b21b7f73719
menglingle <meng_lingle@itrus.com.cn>
2020-09-04 09:47:01 +0800

    更新获取实名流水号

--- src/main/java/com/itrus/common/result/uag/response/entity/UserInfo.java
+++ src/main/java/com/itrus/common/result/uag/response/entity/UserInfo.java
@@ -16,10 +16,10 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 public class UserInfo implements Serializable {
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	/**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    /**
      * 编号
      */
     private String uuid;
@@ -43,9 +43,46 @@ public class UserInfo implements Serializable {
      * 邮箱
      */
     private String email;
-
     /**
      * 创建日期
      */
     private String createTime;
+    /**
+     * 认证结果描述
+     */
+    private String authResultStr;
+    /**
+     * 扩展属性
+     */
+    private String extendAttr;
+    /**
+     * 实名流水ID
+     */
+    private String authId;
+    /**
+     * 证件类型
+     */
+    private Integer idCardType;
+    /**
+     * 证件号码
+     */
+    private String idCardNum;
+    /**
+     * 短信流水ID
+     */
+    private String smsId;
+    /**
+     * 实名类型
+     */
+    private String authType;
+    /**
+     * 实名结果
+     */
+    private String authResult;
+    /**
+     *实名类型字符串
+     */
+    private String authTypeStr;
+
 }
+
